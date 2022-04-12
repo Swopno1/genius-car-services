a6ca13272825b4c867d70cff6834ef12f14f89e0
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-12 23:24:06 +0600

    Added React Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Services from './Pages/Home/Services/Services';
 
 function App() {
   return (
     <div>
       <h1>My Site</h1>
+      <Routes>
+        <Route path='/' element={<Services></Services>}></Route>
+      </Routes>
     </div>
   );
 }
