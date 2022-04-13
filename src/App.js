770c92af55eb9de64bc60a1f72b3c06e5ea41d9c
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-14 05:25:07 +0600

    Push all pending from PC

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
 import Home from './Pages/Home/Home/Home';
 import About from './Pages/About/About';
 
 function App() {
   return (
     <div>
-      <h1>My Site</h1>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Services = () => {
+  const [services, setservices] = useState([]);
+
   return (
     <div>
       <h2>Services</h2>
