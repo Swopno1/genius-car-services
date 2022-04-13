db530bad9204ce1075386a21d582907bc6d12756
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-13 14:38:20 +0600

    Commit from macBook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home/Home/Home';
-import About from './Pages/About/About';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+import About from "./Pages/About/About";
 
 function App() {
   return (
     <div>
-      <h1>My Site</h1>
+      <h1>MacBook: My Site</h1>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='*' element={<Home></Home>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<Home></Home>}></Route>
       </Routes>
     </div>
   );
