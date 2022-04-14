4c527d51072fead73c55ac435771250be347fbfd
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-14 13:03:13 +0600

    Fix image widht issue and nav bar with logo

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,13 +5,13 @@ const Service = ({ service }) => {
   const { name, img, price, description } = service;
   return (
     <div className='service'>
-      <img src={img} alt='' />
+      <img className='w-100' src={img} alt='' />
       <h3>{name}</h3>
       <p>Price: ${price}</p>
       <p>
         <small>{description}</small>
       </p>
-      <button>Book: {name}</button>
+      <button className='btn-primary'>Book: {name}</button>
     </div>
   );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,12 +12,14 @@ const Services = () => {
   }, []);
 
   return (
-    <div className=''>
-      <h1 className='services-title mt-5'>Our Services</h1>
-      <div className='services-container'>
-        {services.map((service) => (
-          <Service key={service.id} service={service}></Service>
-        ))}
+    <div className='container'>
+      <div className='row'>
+        <h1 className='services-title mt-5'>Our Services</h1>
+        <div className='services-container'>
+          {services.map((service) => (
+            <Service key={service.id} service={service}></Service>
+          ))}
+        </div>
       </div>
     </div>
   );
