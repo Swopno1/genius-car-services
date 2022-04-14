8dc59c7bcf5da61e544a0737552d15db7df15188
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-14 12:43:51 +0600

    Added new component Banner, react-bootstrap, and some style corrections.

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -13,7 +13,7 @@ const Services = () => {
 
   return (
     <div className=''>
-      <h1 className='services-title'>Our Services</h1>
+      <h1 className='services-title mt-5'>Our Services</h1>
       <div className='services-container'>
         {services.map((service) => (
           <Service key={service.id} service={service}></Service>
