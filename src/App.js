2502fbdaf1c9b789fc1285b36297f8187baa0000
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-15 03:00:35 +0600

    Added firebase config file, create new component RequreAuth etc.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Home from './Pages/Home/Home/Home';
 import About from './Pages/About/About';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import NotFound from './Pages/Shared/NotFound/NotFound';
-import Login from './Pages/Login/Login';
-import Register from './Pages/Register/Register';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
