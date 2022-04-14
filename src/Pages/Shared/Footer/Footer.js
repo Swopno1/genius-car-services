b72703fe91ea1fce45517f0b2ed6377e564eb63e
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-14 13:16:39 +0600

    Added navbar and other error fix

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className='text-center mt-5'>
+      <p>copyright &copy; {new Date().getFullYear()}</p>
     </footer>
   );
 };
