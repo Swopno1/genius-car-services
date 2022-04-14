982f8fadde3d6682c34be4f4844c489eb115eb25
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-14 09:01:12 +0600

    Styling service and services and add responsiveness

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({ service }) => {
-  const { name, img } = service;
+  const { name, img, price, description } = service;
   return (
-    <div>
+    <div className='service'>
       <img src={img} alt='' />
       <h3>{name}</h3>
+      <p>Price: ${price}</p>
+      <p>
+        <small>{description}</small>
+      </p>
+      <button>Book: {name}</button>
     </div>
   );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
   const [services, setServices] = useState([]);
@@ -11,11 +12,13 @@ const Services = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Services: {services.length}</h2>
-      {services.map((service) => (
-        <Service key={service.id} service={service}></Service>
-      ))}
+    <div className=''>
+      <h1 className='services-title'>Our Services</h1>
+      <div className='services-container'>
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
